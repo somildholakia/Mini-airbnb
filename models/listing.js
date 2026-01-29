923f1b1c79152efccb4ced8b322fb3990ad42518
somildholakia <somildholakia0852@gmail.com>
2026-01-29 19:28:28 +0530

    update

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
     title: {
@@ -32,6 +33,14 @@ const listingSchema = new Schema({
     ],
 
 });
+
+
+listingSchema.post("findOneAndDelete", async(listing) => {
+    if(listing) {
+    await Review.deleteMany({_id: {$in: listing.reviews }});
+}});
+
+
 
 const Listing = mongoose.model("Listing", listingSchema);
 
