19488b7a7c0c779b13c6f21d4bd242f6d1ca5046
somildholakia <somildholakia0852@gmail.com>
2026-01-30 17:21:21 +0530

    update

--- routes/review.js
+++ routes/review.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
+const Review = require("./models/review.js");
 const Listing = require("../models/listing.js");
 const { listingSchema, reviewSchema } = require("../schema.js");
 const ExpressError = require("../utils/ExpressError.js");
