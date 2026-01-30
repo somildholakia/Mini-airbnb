effd1f7e57045fc887485f1f42ae8451ded0eb81
somildholakia <somildholakia0852@gmail.com>
2026-01-30 16:41:37 +0530

    Creating Router for listing page

---
+++ routes/listing.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+
+
+//Index route
+
+app.get("/listings", wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({})
+    res.render("listings/index.ejs", ({ allListings }));
+}));
+
+
+//new route 
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+})
+
+
+
+//show route
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let singleListing = await Listing.findById(id).populate("reviews");
+    console.log(singleListing);
+    res.render("listings/show.ejs", { singleListing });
+}));
+
+// Create Route 
+app.post("/listings", validateListing ,wrapAsync(async (req, res, next) => {
+
+    let { title, description, price, location, country } = req.body;
+    let oneList = await Listing.insertOne({
+        title: title,
+        description: description,
+        price: price,
+        location: location,
+        country: country,
+    });
+
+    console.log(oneList);
+    res.redirect("/listings");
+
+}));
+
+//edit route
+
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+}));
+
+//update route
+
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+
+    if (!req.body.listing) {
+        throw new ExpressError(400, "Send Valid data for listing");
+    }
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect("/listings");
+}));
+
+//DELETE route
+
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+    console.log("Deleted successfully");
+}));
