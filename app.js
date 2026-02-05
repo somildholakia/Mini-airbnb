ebe56be9086b1316cc2e0dd48604f0cb5575bb3f
somildholakia <somildholakia0852@gmail.com>
2026-02-05 15:43:40 +0530

    Added functionality according to user status on authentication buttons on navbar

--- app.js
+++ app.js
@@ -83,13 +83,11 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next) => {
     res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
     next();
 });
 
-app.use((req,res,next) => {
-    res.locals.error = req.flash("error");
-    next();
-});
 
 // app.get("/demo", async (req,res) => {
 //     let fakeUser = new User({
--- routes/user.js
+++ routes/user.js
@@ -16,8 +16,14 @@ router.post("/signup", wrapAsync(async (req, res) => {
         const newUser = new User({ email, username });
         const registeredUser = await User.register(newUser, password);
         console.log(registeredUser);
-        req.flash("success", "User was registered successfully");
-        res.redirect("/listings");
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "User was registered successfully");
+            res.redirect("/listings");
+        })
+
     } catch (err) {
         req.flash("error", err.message);
         res.redirect("/signup");
@@ -27,19 +33,19 @@ router.post("/signup", wrapAsync(async (req, res) => {
 }));
 
 
-router.get("/login", (req,res) => {
+router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 })
 
-router.post("/login", passport.authenticate("local",{failureRedirect: "/login", failureFlash: true}) ,async(req,res) => {
-    req.flash("success","Welcome Back to WandersSite.");
+router.post("/login", passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), async (req, res) => {
+    req.flash("success", "Welcome Back to WandersSite.");
     res.redirect("/listings");
 })
 
-router.get("/logout",(req,res,next) => {
+router.get("/logout", (req, res, next) => {
     req.logout((err) => {
-        if(err) {
-            next(err);
+        if (err) {
+            return next(err);
         }
         req.flash("success", "You are logged Out.");
         res.redirect("/listings");
