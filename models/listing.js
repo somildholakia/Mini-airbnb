e8b6261b4bc79809d275735198c8a175135306b1
somildholakia <somildholakia0852@gmail.com>
2026-02-05 16:45:28 +0530

    update p

--- models/listing.js
+++ models/listing.js
@@ -13,7 +13,10 @@ const listingSchema = new Schema({
     image: {
 
         filename: String,
-        url: String
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        },
 
     },
     price: {
@@ -40,10 +43,11 @@ const listingSchema = new Schema({
 });
 
 
-listingSchema.post("findOneAndDelete", async(listing) => {
-    if(listing) {
-    await Review.deleteMany({_id: {$in: listing.reviews }});
-}});
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
+    }
+});
 
 
 
