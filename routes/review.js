927194da4c9fc5c2dcc1c130455244dab2623114
somildholakia <somildholakia0852@gmail.com>
2026-02-14 19:19:57 +0530

    Created Controller for review

--- routes/review.js
+++ routes/review.js
@@ -5,6 +5,7 @@ const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 const { listingSchema, reviewSchema } = require("../schema.js");
 const ExpressError = require("../utils/ExpressError.js");
+const reviewController = require("../controllers/reviews.js");
 
 
 
@@ -21,19 +22,7 @@ const validateReview = (req,res,next) => {
 
 
 // Reviews POST
-router.post("/", validateReview ,wrapAsync( async (req,res) => {
-
-  let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-        req.flash("success", "New Review Created");
-    res.redirect(`/listings/${req.params.id}`);
-}));
+router.post("/", validateReview ,wrapAsync(reviewController.createReview));
 
 // Delete Review Route
 router.delete("/:reviewId", wrapAsync( async (req,res) =>{
